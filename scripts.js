f207130cae7973cfda01f066d09a2225248fd792
Sebastian Castillo Echeverri <sebascastillo000826@gmail.com>
2020-12-04 23:18:22 -0500

    images and second lever

--- scripts.js
+++ scripts.js
@@ -2,38 +2,31 @@
 var nombre = document.querySelector(".title");
 var btnVal = document.getElementById("validar");
 var ocultar = document.getElementById("entrada");
+var image = document.getElementById("image");
 var respuesta = document.form1.text1;
-var soluciones = ["Na", "Ne","F","P","O","N","C","S","Cl","Al","Cu","Au","Ag"];
-var nombres = ["Sodio", "Neón","Flúor","Fósforo","Oxígeno","Nitrógeno","Carbono","Azufre","Cloro","Aluminio","Cobre","Oro","Plata","Nivel"];
+var soluciones = ["Ne","F","P","O","N","C","S","Cl","Al","Cu","Au","Ag","He","",
+"10","9","15","8","7","6","16","17","13","29","79","47","2","Final"];
+var nombres = ["Neón","Flúor","Fósforo","Oxígeno","Nitrógeno","Carbono","Azufre","Cloro","Aluminio","Cobre","Oro","Plata","Helio","Nivel",
+"Neón","Flúor","Fósforo","Oxígeno","Nitrógeno","Carbono","Azufre","Cloro","Aluminio","Cobre","Oro","Plata","Helio","Final"];
 var pos = 0;
 //add.onClickListener = sodioValidar(document.form1.text1); 
 nombre.innerHTML = nombres[pos];
+console.log(soluciones);
+
+console.log(nombres);
 
 btnVal.addEventListener('click', sodioValidar);
 function sodioValidar( ){ 
       nombre.innerHTML = nombres[pos];
       switch (nombres[pos]) {
-            case "Sodio":
-                  if(respuesta.value.match(soluciones[pos])){ 
-
-                        nombre.innerHTML = nombres[pos+1];
-                        pos += 1;
-                        //alert('¡Muy bien! La que sigue :D');
-                        console.log("Como fue");
-                        respuesta.value = "";
-                  }else{
-                        console.log(":c")
-                        
-                        //alert('Esa no es, intenta de nuevo :c');
-                  }
-            break;
 
             case "Neón":
             
                   console.log(pos);
                   if(respuesta.value.match(soluciones[pos])){
-      
+                        
                         nombre.innerHTML = nombres[pos+1];
+                        document.getElementById("image").src = './images/Flúor.png';
                         pos += 1;
                         console.log(pos);
                         respuesta.value = "";
@@ -47,7 +40,7 @@ function sodioValidar( ){
             case "Flúor":
                   console.log(pos);
                   if(respuesta.value.match(soluciones[pos])){
-      
+                        document.getElementById("image").src = './images/Fósforo.png';
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
                         respuesta.value = "";
@@ -61,7 +54,7 @@ function sodioValidar( ){
             case "Fósforo":
             
                   if(respuesta.value.match(soluciones[pos])){
-      
+                        document.getElementById("image").src = './images/Oxígeno.png';
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
                         respuesta.value = "";
@@ -75,6 +68,7 @@ function sodioValidar( ){
             case "Oxígeno":
             
                   if(respuesta.value.match(soluciones[pos])){
+                        document.getElementById("image").src = './images/Nitrogeno.png';
       
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
@@ -89,6 +83,7 @@ function sodioValidar( ){
             case "Nitrógeno":
             
                   if(respuesta.value.match(soluciones[pos])){
+                        document.getElementById("image").src = './images/Carbono.png';
       
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
@@ -103,7 +98,8 @@ function sodioValidar( ){
             case "Carbono":
             
                   if(respuesta.value.match(soluciones[pos])){
-      
+                        document.getElementById("image").src = './images/Azufre.png';
+
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
                         respuesta.value = "";
@@ -117,6 +113,7 @@ function sodioValidar( ){
             case "Azufre":
             
                   if(respuesta.value.match(soluciones[pos])){
+                        document.getElementById("image").src = './images/Cloro.png';
       
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
@@ -131,7 +128,8 @@ function sodioValidar( ){
             case "Cloro":
             
                   if(respuesta.value.match(soluciones[pos])){
-      
+                        document.getElementById("image").src = './images/Aluminio.png';
+                        
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
                         respuesta.value = "";
@@ -145,6 +143,7 @@ function sodioValidar( ){
             case "Aluminio":
             
                   if(respuesta.value.match(soluciones[pos])){
+                        document.getElementById("image").src = './images/Cobre.png';
       
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
@@ -159,6 +158,7 @@ function sodioValidar( ){
             case "Cobre":
             
                   if(respuesta.value.match(soluciones[pos])){
+                        document.getElementById("image").src = './images/Oro.png';
       
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
@@ -173,6 +173,7 @@ function sodioValidar( ){
             case "Oro":
             
                   if(respuesta.value.match(soluciones[pos])){
+                        document.getElementById("image").src = './images/Plata.png';
       
                         nombre.innerHTML = nombres[pos+1];
                         pos += 1;
@@ -187,6 +188,22 @@ function sodioValidar( ){
             case "Plata":
             
                   if(respuesta.value.match(soluciones[pos])){
+                        document.getElementById("image").src = './images/Helio.png';      
+                        nombre.innerHTML = nombres[pos+1];
+                        pos += 1;
+                        respuesta.value = "";
+                        //alert('¡Muy bien! La que sigue :D');
+                  }else{
+            
+                        //alert('Esa no es, intenta de nuevo :c');
+                  }
+            break;
+
+            case "Helio":
+            
+                  if(respuesta.value.match(soluciones[pos])){
+                        document.getElementById("image").src = './images/win1.png';
+
       
                         document.getElementById("entrada").style.display = "none";
                         nombre.innerHTML = nombres[pos+1];
@@ -200,11 +217,15 @@ function sodioValidar( ){
             break;
 
             case "Nivel":
-
-                  
+                  document.getElementById("image").src = './images/Neón.png';      
+                  document.getElementById("entrada").style.display = "inline-block";
                   nombre.innerHTML = nombres[pos+1];
                   pos += 1;
                   respuesta.value = "";
+            break;
+
+            case "Final":
+
             break;
       }
       
